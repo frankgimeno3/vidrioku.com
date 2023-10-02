a1a4bfbc48a87acfc2a7cdd519b9f14073304650
frank gimeno <frankgimeno3@gmail.com>
2023-10-02 15:37:25 +0200

    implemento bifurcacion en dep tecnico

--- components/searchTree/searchcomponents/algoritmotrabajadores/tipos/dtecnico/dtecnico.tsx
+++ components/searchTree/searchcomponents/algoritmotrabajadores/tipos/dtecnico/dtecnico.tsx
@@ -1,25 +1,31 @@
-import { FC } from "react";
-
+import { FC, useState } from "react";
+import Dtporteducacion from './dtporeducacion'
+import Dtporposicion from './dtporeducacion'
 
 const Dtecnico: FC = () => {
+const [segun, setSegun] = useState("")
+
+const setEducacion = ()=>{
+    setSegun('Educacion')
+}
+const setPosicion = ()=>{
+    setSegun('Posicion')
+
+}
 
 
     return (<>
          <h2 className="pt-12">Según qué criterios desea filtrar los perfiles?</h2>
         <div className="px-2 flex flex-row w-full">
-            <div className="flex flex-col flex-1 p-2">
-                <h2>Según su educación</h2>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento de compras</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento comercial</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento técnico</button>
-            </div>
-            <h2>Según su experiencia</h2>
-            <div className="flex flex-col flex-1 p-2">
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Operarios</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Otros</button>
-            </div>
+ 
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                onClick={setEducacion}>Según su educación</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                onClick={setPosicion}>Por posición</button>
+       
         </div>
+        {segun == "Educacion" && <Dtporteducacion />}
+        {segun == "Posicion" && <Dtporposicion />}
     </>
     );
 };
