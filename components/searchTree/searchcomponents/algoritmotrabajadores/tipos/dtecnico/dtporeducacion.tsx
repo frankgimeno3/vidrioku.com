390586cd72a75b8de16bb6a12174fc5e811e9977
frank gimeno <frankgimeno3@gmail.com>
2023-10-02 15:44:59 +0200

    ajusto opciones dtecnico

--- components/searchTree/searchcomponents/algoritmotrabajadores/tipos/dtecnico/dtporeducacion.tsx
+++ components/searchTree/searchcomponents/algoritmotrabajadores/tipos/dtecnico/dtporeducacion.tsx
@@ -5,12 +5,16 @@ const Dporeducacion: FC = () => {
 
 
     return (<>
-         <h2 className="pt-12">Según qué criterios desea filtrar los perfiles?</h2>
+         <h2 className="pt-12">Qué perfiles le interesan más?</h2>
         <div className="px-2 flex flex-row w-full">
             <div className="flex flex-col flex-1 p-2">
-                 <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento de compras</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento comercial</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento técnico</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Arquitectos, arquitectos técnicos</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros de edificación</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros civiles o de minas</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros técnicos mecánicos</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros técnicos electrónicos</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros industriales</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros informáticos</button>
             </div>
          
         </div>
--- components/searchTree/searchcomponents/algoritmotrabajadores/tipos/dtecnico/dtporposicion.tsx
+++ components/searchTree/searchcomponents/algoritmotrabajadores/tipos/dtecnico/dtporposicion.tsx
@@ -8,9 +8,11 @@ const Dporposicion: FC = () => {
          <h2 className="pt-12">Según qué criterios desea filtrar los perfiles?</h2>
         <div className="px-2 flex flex-row w-full">
             <div className="flex flex-col flex-1 p-2">
-                 <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento de compras</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento comercial</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento técnico</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Director técnico</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Analista de proyectos</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Oficina técnica</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Consultoría técnica</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Mejora contínua</button>
             </div>
   
         </div>
