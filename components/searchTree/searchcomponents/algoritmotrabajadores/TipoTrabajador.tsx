b3ea74fab5cf78a44800bd602a5c924bf233db27
frank gimeno <frankgimeno3@gmail.com>
2023-10-02 15:01:16 +0200

    creo componente con opciones para tipo de trabajador

--- components/searchTree/searchcomponents/algoritmotrabajadores/TipoTrabajador.tsx
+++ components/searchTree/searchcomponents/algoritmotrabajadores/TipoTrabajador.tsx
@@ -1,13 +1,25 @@
 import { FC } from "react";
 
- 
-const TipoOferta: FC = () => {
- 
 
-  return (
-    <h1>Aquí se renderizará un componente que inicie el algoritmo para seleccionar trabajadores</h1>
+const TipoTrabajador: FC = () => {
 
-  );
+
+    return (<>
+         <h2 className="pt-12">Qué tipo de perfil necesita?</h2>
+        <div className="px-2 flex flex-row w-full">
+            <div className="flex flex-col flex-1 p-2">
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento de compras</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento comercial</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento técnico</button>
+            </div>
+            <div className="flex flex-col flex-1 p-2">
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Operarios</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Otros</button>
+            </div>
+        </div>
+    </>
+    );
 };
 
-export default TipoOferta;
+export default TipoTrabajador;
