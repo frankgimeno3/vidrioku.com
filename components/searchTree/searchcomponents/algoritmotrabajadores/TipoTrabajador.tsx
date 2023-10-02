1f5381d63312f6b2ca695bbc19c05bbdac965e17
frank gimeno <frankgimeno3@gmail.com>
2023-10-02 15:25:37 +0200

    creo asociacion componentes desglose

--- components/searchTree/searchcomponents/algoritmotrabajadores/TipoTrabajador.tsx
+++ components/searchTree/searchcomponents/algoritmotrabajadores/TipoTrabajador.tsx
@@ -1,23 +1,60 @@
-import { FC } from "react";
-
+import React, { FC, useState } from 'react';
+import Compras from './tipos/comercial/Comercial'
+import Comercial from './tipos/comercial/Comercial'
+import Dtecnico from './tipos/comercial/Comercial'
+import Ingenieros from './tipos/comercial/Comercial'
+import Operarios from './tipos/operarios/departamentos/Operarios'
+import Otros from './tipos/Otros'
 
 const TipoTrabajador: FC = () => {
+    const [tipoTrabajador, setTipoTrabajador] = useState('');
 
+    const setCompras = () => {
+        setTipoTrabajador('Compras')
+    }
+    const setComercial = () => {
+        setTipoTrabajador('Comercial')
+    }
+    const setDtecnico = () => {
+        setTipoTrabajador('Dtecnico')
+    }
+    const setIngenieros = () => {
+        setTipoTrabajador('Ingenieros')
+    }
+    const setOperarios = () => {
+        setTipoTrabajador('Operarios')
+    }
+    const setOtros = () => {
+        setTipoTrabajador('Otros')
+    }
 
     return (<>
-         <h2 className="pt-12">Qué tipo de perfil necesita?</h2>
+        <h2 className="pt-12">Qué tipo de perfil necesita?</h2>
         <div className="px-2 flex flex-row w-full">
             <div className="flex flex-col flex-1 p-2">
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento de compras</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento comercial</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Departamento técnico</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                    onClick={setCompras}>Departamento de compras</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                    onClick={setComercial}>Departamento comercial</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                    onClick={setDtecnico}>Departamento técnico</button>
             </div>
             <div className="flex flex-col flex-1 p-2">
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Ingenieros</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Operarios</button>
-                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2">Otros</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                    onClick={setIngenieros}>Ingenieros</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                    onClick={setOperarios}>Operarios</button>
+                <button className="bg-zinc-100 text-zinc-700 shadow-lg p-2 text-xs h-16 my-2"
+                    onClick={setOtros}>Otros</button>
             </div>
         </div>
+        {tipoTrabajador == "Compras" && <Compras />}
+        {tipoTrabajador == "Comercial" && <Comercial />}
+        {tipoTrabajador == "Dtecnico" && <Dtecnico />}
+        {tipoTrabajador == "Ingenieros" && <Ingenieros />}
+        {tipoTrabajador == "Operarios" && <Operarios />}
+        {tipoTrabajador == "Otros" && <Otros />}
+
     </>
     );
 };
--- components/searchTree/searchcomponents/algoritmotrabajadores/tipos/operarios/departamentos.tsx
+++
@@ -1,6 +0,0 @@
-<>
-<h1>ESTOS TINENEN QUE IR POR DEPARTAMENTOS QUE DESGLOSEN CARGOS ESPECÍFICOS</h1>
-
-
-<h2>Luego, ya abriremos el melón de la experiencia</h2>
-</>
