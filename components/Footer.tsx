1aa5422440b874406b4694eab7f48e693f09051e
frank gimeno <frankgimeno3@gmail.com>
2023-10-03 17:46:57 +0200

    modifico footer y añado cabezales oscuros a todas las pags

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,38 +17,41 @@ const Footer: React.FC<FooterProps> = ({ onPageChange }) => {
 
 
   return (
-    <div className="bg-blue-50 shadow-lg p-4 pt-12 flex justify-between  border-gray-300">
-      <div className="mx-10 lg:mx-24 text-gray-600 relative z-10">
+    <div className="bg-gray-50 shadow-lg p-4 pt-12 flex justify-center  border-gray-300">
+      <div className="mx-5 lg:mx-24 text-gray-600 relative z-10">
         <div className="flex flex-col sm:flex-row  items-top justify-between align-top">
-          <div className="lg:flex-1">
-            <h3 className="text-sm font-semibold">Sobre Nosotros</h3>
+          <div className="flex-1">
+            <h3 className="text-sm text font-semibold">Sobre Nosotros</h3>
             <p className="text-xs mt-2">
               Somos una agencia de desarrolladores web con sede en Barcelona, comprometidos en ofrecer soluciones de desarrollo de software de alta calidad. Nuestro equipo está formado por expertos en diferentes tecnologías y estamos dedicados a ayudar a nuestros clientes a alcanzar sus objetivos tecnológicos.
             </p>
             <p className="text-xs">
               Actualmente, formamos parte del grupo PROPORCION3,S.A. medios de comunicación .</p>
           </div>
-          <div className="lg:flex-1 text-left py-10 sm:py-1" >
-            <h3 className="text-sm font-semibold">Enlaces Rápidos</h3>
-            <div className="  flex flex-col  text-xs">
-              <button className="mt-2 text-left md:text-center" onClick={() => redirection('Home')} >Inicio</button>
-              <button className="mt-3 text-left md:text-center" onClick={() => redirection('QuienesSomos')} >Nosotros</button>
-              <button className="mt-3 text-left md:text-center" onClick={() => redirection('Servicios')} >Servicios</button>
-              <button className="mt-3 text-left md:text-center" onClick={() => redirection('Contacto')} >Contacto</button>
+          <div className='flex flex-row py-5'>
+            <div className="flex-1 text-left sm:py-1" >
+              <h3 className="text-sm font-semibold">Enlaces Rápidos</h3>
+              <div className="flex flex-col  text-xs">
+                <button className="mt-2 text-left md:text-center" onClick={() => redirection('Home')} >Inicio</button>
+                <button className="mt-3 text-left md:text-center" onClick={() => redirection('QuienesSomos')} >Nosotros</button>
+                <button className="mt-3 text-left md:text-center" onClick={() => redirection('Servicios')} >Servicios</button>
+                <button className="mt-3 text-left md:text-center" onClick={() => redirection('Contacto')} >Contacto</button>
+              </div>
             </div>
-          </div>
-          <div className="lg:flex-1">
-            <h3 className="text-md font-semibold">Contáctanos</h3>
-            <address className="flex flex-col text-sm mt-2">
-              <p>Dirección: Bruc 48, Barcelona</p>
-              <p>Teléfono: (+34) *** *** ***</p>
-              <p>Email: info@vidrioku.es</p>
-            </address>
-            <Image src="/logos/3.png" alt="Logo3" width={50} height={50} />
+            <div className=" flex-1">
+              <h3 className="text-sm font-semibold">Contáctanos</h3>
+              <address className="flex flex-col text-xs mt-2">
+                <p className='pt-1'>Dirección: Bruc 48, Barcelona</p>
+                 <p className='pt-1'>Teléfono: (+34) *** *** ***</p>
+                 <p className='pt-1'>Email: info@vidrioku.es</p>
+              </address>
+            </div>
           </div>
         </div>
         <div className="mt-4 text-center text-zinc-800 text-sm">
           &copy; 2023 PROPORCION3,S.A. | Todos los derechos reservados
+          <Image src="/logos/3.png" alt="Logo3" width={50} height={50} className='mx-auto pt-5'/>
+
         </div>
       </div>
     </div>
--- components/screens/Notifications.tsx
+++ components/screens/Notifications.tsx
@@ -15,8 +15,8 @@ const Notifications: FC = ({  }) => {
    
 
    return (
-    <div className="h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
-      <h2 className="bg-zinc-800 mx-6 font-bold text-lg px-5 py-3">Notificaciones</h2>
+    <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
+    <h2 className="bg-zinc-800  bg-white bg-opacity-50 font-bold text-lg  py-3 text-center">Notificaciones</h2>
     <div className=" mx-6  bg-white  h-full text-zinc-900">
       <div className="p-5">
 
