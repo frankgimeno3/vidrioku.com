fc552985e75a7b766956b1494be588d01c7e2bfa
frank gimeno <frankgimeno3@gmail.com>
2023-10-04 10:51:12 +0200

    creo contenido notificaciones

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -17,28 +17,28 @@ const HOME: FC = ({ }) => {
   return (
     <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
       <h2 className="bg-zinc-800  bg-white bg-opacity-50 font-bold text-lg  py-3 text-center">Bienvenido, {usuario}</h2>
-      
-      <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100 my-5 rounded-lg">
-        <div className='px-12 py-5 '>
-          <h2 className='text-center font-bold '>Notificaciones</h2>
-          <h2 className='mt-3 text-sm mb-2'>Tienes <span className='font-bold'>4</span> notificaciones nuevas</h2>
+      <div className='flex flex-col '>
+        <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100  rounded-lg my-6 mt-6">
+          <div className='px-12 py-5 '>
+            <h2 className='text-center font-bold '>Notificaciones</h2>
+            <h2 className='mt-3 text-sm mb-2'>Tienes <span className='font-bold'>4</span> notificaciones nuevas</h2>
+          </div>
+        </div>
+
+        <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100  rounded-lg my-6">
+          <div className='text-center py-5'>
+            <h2 className='text-center font-bold '>Mensajes</h2>
+            <h2 className='mt-3 text-sm mb-2'>Tienes <span className='font-bold'>3 </span>mensajes de <span className='font-bold'>2</span> conversaciones</h2>
+          </div>
+        </div>
+
+        <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100  rounded-lg my-6">
+          <div className='text-center py-5'>
+            <h2 className='text-center font-bold '>Seguimientos</h2>
+            <h2 className='mt-3 text-sm mb-2 px-7'>Hay <span className='font-bold'>2 </span>nuevas ofertas que se ajustan a tu perfil</h2>
+          </div>
         </div>
       </div>
-
-      <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100 my-5 rounded-lg">
-        <div className='text-center py-5'>
-          <h2 className='text-center font-bold '>Mensajes</h2>
-          <h2 className='mt-3 text-sm mb-2'>Tienes <span className='font-bold'>3 </span>mensajes de <span className='font-bold'>2</span> conversaciones</h2>
-        </div>
-      </div>
-
-      <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100 my-5 rounded-lg">
-        <div className='text-center py-5'>
-          <h2 className='text-center font-bold '>Seguimientos</h2>
-          <h2 className='mt-3 text-sm mb-2 px-7'>Hay <span className='font-bold'>2 </span>nuevas ofertas que se ajustan a tu perfil</h2>
-        </div>
-      </div>
-
     </div>
   );
 };
--- components/screens/Notifications.tsx
+++ components/screens/Notifications.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/router'; 
+import { useRouter } from 'next/router';
 
 // interface NavbarProps {
 //   currentComponent: string;
@@ -9,17 +9,41 @@ import { useRouter } from 'next/router';
 
 // const Navbar: FC<NavbarProps> = ({ currentComponent, setCurrentComponent }) => {
 
-const Notifications: FC = ({  }) => {
-   const router = useRouter();  
+const Notifications: FC = ({ }) => {
+  const router = useRouter();
 
-   
 
-   return (
+
+  return (
     <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
-    <h2 className="bg-zinc-800  bg-white bg-opacity-50 font-bold text-lg  py-3 text-center">Notificaciones</h2>
-    <div className=" mx-6  bg-white  h-full text-zinc-900">
-      <div className="p-5">
+      <h2 className="bg-zinc-800  bg-white bg-opacity-50 font-bold text-lg  py-3 text-center">Notificaciones</h2>
+      <div className='flex flex-col '>
+        <div className="  mx-6  bg-white bg-opacity-5  text-zinc-100  rounded-lg my-6 mt-6">
+          <div className='px-8 pt-5 '>
+            <h2 className='mt-2 text-md text-center  '>Tienes <span className='font-bold'>4</span> notificaciones nuevas</h2>
+          </div>
 
+          <div className="  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-6">
+            <h2 className='text-right pr-3 pt-2 text-gray-400 text-sm'>Mensaje</h2>
+            <h2 className='mt-1 text-sm mb-2 mx-5'>La empresa
+              <span className='font-bold'> VIDRIOPERFIL </span>te ha enviado un mensaje </h2>
+          </div>
+
+          <div className="  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-6">
+            <h2 className='text-right pr-3 pt-2 text-gray-400 text-sm'>Oportunidad</h2>
+            <h2 className='mt-1 text-sm mb-2 mx-5'>Hay
+              <span className='font-bold'> 3 </span>nuevas ofertas de trabajo, que encajan con tu perfil en Barcelona </h2>
+          </div>
+          <div className="  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-6">
+            <h2 className='text-right pr-3 pt-2 text-gray-400 text-sm'>Seguimientos</h2>
+            <h2 className='mt-1 text-sm mb-2 mx-5'>La empresa
+              <span className='font-bold'> GLASTON </span> ha publicado 3 nuevas ofertas de empleo</h2>
+          </div>
+          <div className="  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-6">
+            <h2 className='text-right pr-3 pt-2 text-gray-400 text-sm'>Mensaje</h2>
+               <h2 className='mt-1 text-sm mb-2 mx-5'>El usuario
+               <span className='font-bold'> Alex Gimeno </span>te ha enviado un mensaje </h2>
+           </div>
         </div>
       </div>
     </div>
